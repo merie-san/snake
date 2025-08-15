6ee3b89253f257d7a1bdc4cf7cc7fcf6a42a91d7
merie-san <davide.zhang2@gmail.com>
2025-08-15 11:40:24 +0200

    added entity classes for Hibernate and a factory class for creating entity objects

---
+++ snake/src/main/java/com/minigames/snake/GameRecord.java
@@ -0,0 +1,23 @@
+package com.minigames.snake;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+
+@Entity
+public class GameRecord extends BaseEntity {
+
+	private int score;
+	private LocalDate date;
+	@ManyToOne
+	private GameSetting setting;
+
+	// for JPA
+	protected GameRecord() {
+		super();
+	}
+
+	public GameRecord(String uuid) {
+		super(uuid);
+	}
+
+}
